85dde6fa9f9dce1ec41ea24d34dab71456445e9d
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-02-22 16:33:28 -0500

    Refactor Jobber to own module, create scripts in outputdir

--- control/action.py
+++ control/action.py
@@ -1,181 +1,18 @@
 str_about = '''
     The action module provides functionality to run individual
-    plugins as well as "pipelines" of plugins. It is the interface
-    between the plugin and a CUBE API via the CLI.
+    plugins as well as "pipelines" of plugins.
+
+    This module is the contact "surface" between dypi and a CUBE
+    instance. Control/manipulation of the ChRIS instance is effected
+    by a set of CLI scripts that this module creates and then executes.
 '''
 
-import  subprocess
+from    .                       import  jobber
 import  os
 import  re
 import  pudb
 import  json
-
-class jobber:
-
-    def __init__(self, d_args : dict):
-        """Constructor for the jobber class.
-
-        Args:
-            d_args (dict): a dictionary of "arguments" (parameters) for the
-                           object.
-        """
-        self.args   = d_args.copy()
-        if not 'verbosity'      in self.args.keys(): self.args['verbosity']     = 0
-        if not 'noJobLogging'   in self.args.keys(): self.args['noJobLogging']  = False
-
-    def dict2JSONcli(self, d_dict : dict) -> str:
-        """Convert a dictionary into a CLI conformant JSON string.
-
-        An input dictionary of
-
-            {
-                'key1': 'value1',
-                'key2': 'value2'
-            }
-
-        is converted to a string:
-
-            "{\"key1\":\"value1\",\"key2\":\"value2\"}"
-
-        Args:
-            d_dict (dict): a python dictionary to convert
-
-        Returns:
-            str: CLI equivalent string.
-        """
-
-        str_JSON    = json.dumps(d_dict)
-        str_JSON    = str_JSON.replace('"', r'\"')
-        return str_JSON
-
-    def dict2cli(self, d_dict : dict) -> str:
-        """Convert a dictionary into a CLI conformant JSON string.
-
-        An input dictionary of
-
-            {
-                'key1': 'value1',
-                'key2': 'value2',
-                'key3': true,
-                'key4': false
-            }
-
-        is converted to a string:
-
-            "--key1 value1 --key2 value2 --key3"
-
-        Args:
-            d_dict (dict): a python dictionary to convert
-
-        Returns:
-            str: CLI equivalent string.
-        """
-        str_cli     : str = ""
-        for k,v in d_dict.items():
-            if type(v) == bool:
-                if v:
-                    str_cli += '--%s ' % k
-            elif len(v):
-                str_cli += '--%s %s ' % (k, v)
-        return str_cli
-
-    def job_run(self, str_cmd):
-        """
-        Running some CLI process via python is cumbersome. The typical/easy
-        path of
-                            os.system(str_cmd)
-        is deprecated and prone to hidden complexity. The preferred
-        method is via subprocess, which has a cumbersome processing
-        syntax. Still, this method runs the `str_cmd` and returns the
-        stderr and stdout strings as well as a returncode.
-        Providing readtime output of both stdout and stderr seems
-        problematic. The approach here is to provide realtime
-        output on stdout and only provide stderr on process completion.
-        """
-        d_ret       : dict = {
-            'stdout':       "",
-            'stderr':       "",
-            'cmd':          "",
-            'cwd':          "",
-            'returncode':   0
-        }
-        str_stdoutLine  : str   = ""
-        str_stdout      : str   = ""
-
-        p = subprocess.Popen(
-                    str_cmd.split(),
-                    stdout      = subprocess.PIPE,
-                    stderr      = subprocess.PIPE,
-        )
-
-        # Realtime output on stdout
-        while True:
-            stdout      = p.stdout.readline()
-            if p.poll() is not None:
-                break
-            if stdout:
-                str_stdoutLine = stdout.decode()
-                if int(self.args['verbosity']):
-                    print(str_stdoutLine, end = '')
-                str_stdout      += str_stdoutLine
-        d_ret['cmd']        = str_cmd
-        d_ret['cwd']        = os.getcwd()
-        d_ret['stdout']     = str_stdout
-        d_ret['stderr']     = p.stderr.read().decode()
-        d_ret['returncode'] = p.returncode
-        with open('/tmp/job.json', 'w') as f:
-            json.dump(d_ret, f, indent=4)
-        if int(self.args['verbosity']) and len(d_ret['stderr']):
-            print('\nstderr: \n%s' % d_ret['stderr'])
-        return d_ret
-
-    def job_runbg(self, str_cmd : str) -> dict:
-        """Run a job in the background
-
-        Args:
-            str_cmd (str): CLI string to run
-
-        Returns:
-            dict: a dictionary of exec state
-        """
-        d_ret       : dict = {
-            'uid'       : "",
-            'cmd'       : "",
-            'cwd'       : ""
-        }
-        # str_stdoutLine  : str   = ""
-        # str_stdout      : str   = ""
-
-        p = subprocess.Popen(
-                    str_cmd.split(),
-                    stdout      = subprocess.PIPE,
-                    stderr      = subprocess.PIPE,
-        )
-
-        d_ret['uid']        = str(os.getuid())
-        d_ret['cmd']        = str_cmd
-        d_ret['cwd']        = os.getcwd()
-        # d_ret['stdout']     = str_stdout
-        # d_ret['stderr']     = p.stderr.read().decode()
-        # d_ret['returncode'] = p.returncode
-        # if int(self.args['verbosity']) and len(d_ret['stderr']):
-        #     print('\nstderr: \n%s' % d_ret['stderr'])
-        return d_ret
-
-    def job_stdwrite(self, d_job, str_outputDir, str_prefix = ""):
-        """
-        Capture the d_job entries to respective files.
-        """
-        if not self.args['noJobLogging']:
-            for key in d_job.keys():
-                with open(
-                    '%s/%s%s' % (str_outputDir, str_prefix, key), "w"
-                ) as f:
-                    f.write(str(d_job[key]))
-                    f.close()
-        return {
-            'status': True
-        }
+from    argparse                import ArgumentParser, Namespace
 
 class PluginRun:
     '''
@@ -183,13 +20,18 @@ class PluginRun:
     to CUBE.
     '''
     def __init__(self, *args, **kwargs):
-        self.env                = None
-        self.plugin             = ''
-        self.shell              : jobber    = jobber({'verbosity' : 1, 'noJobLogging': True})
-        self.attachToPluginID   : str       = ''
+        self.env                                    = None
+        self.plugin                                 = ''
+        self.shell              : jobber.Jobber     = jobber.Jobber({
+                                                        'verbosity' :   1,
+                                                        'noJobLogging': True
+                                                        })
+        self.attachToPluginID   : str               = ''
+        self.options            : Namespace         = None
         for k, v in kwargs.items():
-            if k == 'attachToPluginID'  :   self.attachToPluginID   = v
-            if k == 'env'               :   self.env                = v
+            if k == 'attachToPluginID'  : self.attachToPluginID     = v
+            if k == 'env'               : self.env                  = v
+            if k == 'options'           : self.options              = v
 
         self.l_runCMDresp       : list  = []
         self.l_branchInstanceID : list  = []
@@ -206,7 +48,7 @@ class PluginRun:
             --title=%s;
             --previous_id=%s
         """ % (str_input, str_input, self.env.parentPluginInstanceID)
-        str_args = re.sub(r';\n.*--', ';--', str_args) 
+        str_args = re.sub(r';\n.*--', ';--', str_args)
         str_args = str_args.strip()
         return {
             'args':     str_args
@@ -240,10 +82,15 @@ class PluginRun:
         # Remove the '/incoming/' from the str_input
         str_inputTarget     : str   = str_input.split('/')[-1]
         d_PLCmd             : dict  = self.chrispl_run_cmd(str_inputTarget)
-        str_PLCmd           : str   = d_PLCmd['cmd'] 
+        str_PLCmd           : str   = d_PLCmd['cmd']
         str_PLCmdfile       : str   = '/tmp/%s.sh' % str_inputTarget
         branchID            : int   = -1
         b_status            : bool  = False
+
+        if self.options:
+            str_scriptDir   : str   = '%s/%s' % (self.options.outputdir, str_inputTarget)
+            os.mkdir(str_scriptDir)
+            str_PLCmdfile   = '%s/%s/copy.sh' % (self.options.outputdir, str_inputTarget)
 
         with open(str_PLCmdfile, 'w') as f:
             f.write('#!/bin/bash\n')
@@ -261,6 +108,7 @@ class PluginRun:
         return {
             'status'            : b_status,
             'run'               : d_runCMDresp,
+            'input'             : str_input,
             'branchInstanceID'  : branchID
         }
 
@@ -271,13 +119,18 @@ class Caw:
     '''
 
     def __init__(self, *args, **kwargs):
-        self.env                = None
-        self.pipeline           : str       = ''
-        self.shell              : jobber    =  jobber({'verbosity' : 1, 'noJobLogging': True})
-        self.attachToPluginID   : str       = ''
+        self.env                                    = None
+        self.pipeline           : str               = ''
+        self.shell              : jobber.Jobber     = jobber.Jobber({
+                                                        'verbosity' :   1,
+                                                        'noJobLogging': True
+                                                        })
+        self.attachToPluginID   : str               = ''
+        self.options            : Namespace         = None
         for k, v in kwargs.items():
-            if k == 'attachToPluginID'  :   self.attachToPluginID   = v
-            if k == 'env'               :   self.env                = v
+            if k == 'attachToPluginID'  : self.attachToPluginID     = v
+            if k == 'env'               : self.env                  = v
+            if k == 'options'           : self.options              = v
         self.l_runCMDresp       : list  = []
 
     def pipeline(self, *args) -> str:
@@ -301,7 +154,8 @@ class Caw:
             'args': str_args
         }
 
-    def caw_run_cmd(self, attachToPluginID: int, pipeline: str) -> dict:
+    def caw_run_cmd(self,   attachToPluginID    : int,
+                            pipeline            : str) -> dict:
         '''
         Return the CLI for the caw call
         '''
@@ -313,13 +167,20 @@ class Caw:
             'cmd' : str_cmd
         }
 
-    def __call__(self, filteredCopyInstanceID : dict, str_pipeline : str) -> dict:
+    def __call__(self,      filteredCopyInstanceID  : int,
+                            str_pipeline            : str,
+                            str_input               : str = "") -> dict:
         '''
         Call caw on the appropriate plugin instance ID
         '''
         d_cawCmd            : dict  = self.caw_run_cmd(filteredCopyInstanceID, str_pipeline)
-        str_cawCmd          : str   = d_cawCmd['cmd'] 
+        str_cawCmd          : str   = d_cawCmd['cmd']
         str_cawCmdfile      : str   = '/tmp/caw-%s.sh' % filteredCopyInstanceID
+
+        if self.options:
+            str_cawCmdfile  = '%s/%s/caw-%s.sh' % ( self.options.outputdir,
+                                                    str_input,
+                                                    filteredCopyInstanceID)
 
         with open(str_cawCmdfile, 'w') as f:
             f.write('#!/bin/bash\n')
@@ -329,4 +190,4 @@ class Caw:
         return {
             'response'      : d_runCMDresp
         }
-       
+
